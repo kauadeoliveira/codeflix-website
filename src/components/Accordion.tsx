a99d69473ee4c5da44f48044d4d519e37dc41768
Kauã de Oliveira <98750284+kauadeoliveira@users.noreply.github.com>
2023-03-10 15:02:50 -0300

    feat: implement the Accordion component

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { ChildrenType } from '@/types/children';
+import { useState } from 'react';
+
+type AccordionType = {
+    title: string;
+    children: ChildrenType;
+}
+
+
+export default function Accordion({ title, children }: AccordionType) {
+    const [openAccordion, setOpenAccordion] = useState<boolean>(false);
+    const handleOpenAccordion = () => setOpenAccordion(!openAccordion);
+
+    return(
+        <div>
+            <button onClick={handleOpenAccordion} className='uppercase font-bold'>
+                {title}
+            </button>
+            <div className={`${openAccordion ? "block" : "hidden"} text-base px-2`}>
+                {children}
+            </div>
+        </div>
+    )
+}
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,27 +1,27 @@
 "use client";
 
 import { MyContext } from '@/context';
-import { categories } from '@/utils/categories';
 import { useContext } from 'react';
-import { HiX } from "react-icons/hi"
+import { HiX,} from "react-icons/hi"
+import Accordion from './Accordion';
 import MenuItem from './MenuItem';
+
 
 export default function Menu() {
     const { openMenu, setOpenMenu } = useContext(MyContext);
-    
     const handleCloseMenu = () => setOpenMenu?.(false);
 
     return(
         <div 
-         className={`block h-screen w-full fixed top-0 left-0 ${openMenu ? "transform-none" : "translate-y-[-100vh]"}
-         duration-500 transition-transform bg-black p-3 md:hidden`}
+         className={`block h-screen w-full ${openMenu ? "transform-none" : "translate-y-[-100vh]"}
+         duration-500 transition-transform bg-black p-3 md:hidden fixed top-0 left-0`}
         >
             <div className='flex justify-end'>
                 <button onClick={handleCloseMenu}>
                     <HiX />
                 </button>
             </div>
-            <div className='text-2xl flex flex-col gap-3'>
+            <nav className='text-2xl flex flex-col gap-3 '>
                 <MenuItem>
                     <a href="#">My List</a>
                 </MenuItem>
@@ -31,8 +31,10 @@ export default function Menu() {
                 <MenuItem>
                     <a href="#">Series</a>
                 </MenuItem>
-                <MenuItem>Categories</MenuItem>
-            </div>
+                <Accordion title='Categories'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dicta inventore aut quae suscipit, ex consequuntur deserunt doloribus magnam blanditiis alias maiores nemo excepturi sapiente? Debitis distinctio quos fugit maxime.
+                </Accordion>
+            </nav>
         </div>
     )
 }
